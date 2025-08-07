4379de2350b8beee5c8492d6cac2731f0ca35eb9
Ona0nline <onasihle123@gmail.com>
2025-08-07 13:54:44 +0200

    Needed secrets

--- adgoMain/models.py
+++ adgoMain/models.py
@@ -1,3 +1,15 @@
+# adgoMain/models.py
 from django.db import models
 
-# Create your models here.
+class ChannelStats(models.Model):
+    channel_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    subscriber_count = models.BigIntegerField()
+    view_count = models.BigIntegerField()
+    video_count = models.BigIntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
