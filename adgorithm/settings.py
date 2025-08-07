6ca5827f15532af8171ad7fc67d432dacaf55fde
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2025-08-07 21:34:29 +0200

    added new allowed hosts path to project settings file

--- adgorithm/settings.py
+++ adgorithm/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-6-6$ade4&!i(3k%a7)m0@t$0=z-&hm)b7ec+x%#mfh^69u+2ku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
