1d9f097c0fd1fbcd4ebb1af38daeab69b9dd6320
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2025-08-07 19:07:12 +0200

    fixed styling connection

--- adgoMain/urls.py
+++ adgoMain/urls.py
@@ -6,10 +6,15 @@ from . import views
 app_name = 'adgoMain'
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    # path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     # path('auth/', views.youtube_auth, name='youtube_auth'),
     # path('stats/', views.channel_stats, name='channel_stats'),
     path('auth/start/', views.youtube_auth_start, name='youtube_auth_start'),
     path('oauth2callback/', views.youtube_auth_callback, name='youtube_auth_callback'),
     path('stats/', views.channel_stats, name='channel_stats'),
+
     
 ]
--- adgoMain/views.py
+++ adgoMain/views.py
@@ -4,8 +4,20 @@ from django.conf import settings
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 import google.oauth2.credentials
+from django.contrib.auth import authenticate, login, logout
+
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from django import forms
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+
+def index(request):
+    return render(request, "adgoMain/index.html")
 
 def youtube_auth_start(request):
     flow = Flow.from_client_secrets_file(
@@ -80,3 +92,56 @@ def channel_stats(request):
     }
 
     return render(request, 'adgoMain/channel_stats.html', context)
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "adgoMain/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "adgoMain/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "adgoMain/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "adgoMain/register.html")
+
+    #creating a new listing
